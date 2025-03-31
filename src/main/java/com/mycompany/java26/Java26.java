d7dc1086a1ee020b50f0b0cee0fd751e4816af77
10725116225 <10725116225@UNS07DT98857.animaedu.intranet>
2025-03-31 11:05:45 -0300

    inicio do projeto

---
+++ src/main/java/com/mycompany/java26/Java26.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java26;
+
+/**
+ *
+ * @author 10725116225
+ */
+public class Java26 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
